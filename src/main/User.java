fd218a56abe622ad9f5a8652eeb898acfb4ee6b0
ChanNyein <uchannyein5@gmail.com>
2023-06-19 10:26:54 +0630

    updated add gitignore file and changed vi editor

--- src/main/User.java
+++ src/main/User.java
@@ -4,5 +4,6 @@ public class User {
 	public static void main(String[] args) {
 		System.out.println("just git test");
 		System.out.println("changes");
+		System.out.println("just modified");
 	}
 }
